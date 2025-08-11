ab99893f605e7f7c7203d0727970b1bc3dc01b94
Flavius00 <flaviusturcu11@gmail.com>
2025-08-11 14:45:16 +0300

    added edit family functionality as well as kicking a user and updating user roles if admin functionality

---
+++ src/Form/FamilyEditFormType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Family;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FamilyEditFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('monthlyTargetBudget')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Family::class,
+        ]);
+    }
+}
