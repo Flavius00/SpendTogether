241cc2ab511ce84787ca3e3596496dc80f6e77ad
Flavius Turcu <flaviusturcu11@gmail.com>
2025-08-11 10:05:59 +0300

    missing the add membre functionality

--- src/Controller/FamilyController.php
+++ src/Controller/FamilyController.php
@@ -6,46 +6,55 @@ use App\Entity\Family;
 use App\Entity\User;
 use App\Form\CreateFamilyFormType;
 use App\Repository\FamilyRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Transport\Smtp\Auth\LoginAuthenticator;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route('/family')]
 final class FamilyController extends AbstractController
 {
-    #[Route('/', name: 'app_family_home')]
+    #[Route('/home', name: 'app_family_home')]
     public function index(
-        AuthorizationCheckerInterface $authorizationChecker,
+        #[CurrentUser]
+        User $user,
+        AuthorizationCheckerInterface $authCheck,
     ): Response
     {
-        if (!$authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+        if (!$authCheck->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             return $this->redirectToRoute('app_login');
         }
 
-        $user = $this->getUser();
         if (is_null($user->getFamily())) {
             return $this->redirectToRoute('app_family_create');
         }
-        return $this->render('family/index.html.twig', [
+
+        return $this->render('family/home.html.twig', [
             'userEmail' => $user->getEmail(),
+            'family' => $user->getFamily(),
         ]);
     }
 
     #[Route('/create', name: 'app_family_create')]
     public function createFamilyPage(
+        #[CurrentUser]
+        User $user,
         Request $request,
-        AuthorizationCheckerInterface $authorizationChecker,
+        AuthorizationCheckerInterface $authCheck,
         EntityManagerInterface $em,
+        Security $login,
     ) : Response
     {
-        if (!$authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+        if (!$authCheck->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             return $this->redirectToRoute('app_login');
         }
 
-        $user = $this->getUser();
         if (!is_null($user->getFamily())) {
             return $this->redirectToRoute('app_family_home');
         }
@@ -60,9 +69,11 @@ final class FamilyController extends AbstractController
             $em->flush();
 
             $user->setFamily($family);
-            $user->setRole(['FAMILY_ADMIN']);
+            $user->setRoles(['ROLE_ADMIN']);
             $em->persist($user);
             $em->flush();
+
+            $login->login($user);
 
             return $this->redirectToRoute('app_family_home');
         }
@@ -75,21 +86,47 @@ final class FamilyController extends AbstractController
 
     #[Route('/join', name: 'app_family_join')]
     public function getFamily(
-        AuthorizationCheckerInterface $authorizationChecker,
+        #[CurrentUser]
+        User $user,
+        AuthorizationCheckerInterface $authCheck,
         FamilyRepository $familyRepository,
     ) : Response
     {
-        if (!$authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+        if (!$authCheck->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
             return $this->redirectToRoute('app_login');
         }
 
         $families = $familyRepository->findAll();
-        $user = $this->getUser();
-
 
         return $this->render('family/join.html.twig', [
             'families' => $families,
             'userEmail' => $user->getEmail(),
+        ]);
+    }
+
+    #[Route('/show-free-users', name: 'app_family_show_free_users')]
+    public function getFreeUsers(
+        #[CurrentUser]
+        User $user,
+        AuthorizationCheckerInterface $authCheck,
+        UserRepository $userRepository,
+        Security $security,
+    ) : Response
+    {
+        if (!$authCheck->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if (!$security->isGranted('ROLE_ADMIN')) {
+            $this->addFlash("error", "Restricted acccess. You are not allowed to add users to the family.");
+            return $this->redirectToRoute('app_family_home');
+        }
+
+        $users = $userRepository->findBy(['family' => null]);
+        return $this->render('family/search-free-user.html.twig', [
+            'users' => $users,
+            'userEmail' => $user->getEmail(),
+            'family' => $user->getFamily(),
         ]);
     }
 }
