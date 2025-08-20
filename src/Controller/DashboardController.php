2c9fb9c4114441d92ad6a4ef9731b158e67efe11
Flavius00 <flaviusturcu11@gmail.com>
2025-08-20 14:46:37 +0300

    small added fixtures

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/dashboard')]
 final class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('', name: 'app_dashboard')]
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
